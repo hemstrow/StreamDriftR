19308ad57fc507aa788e25adb103a26c8a76aa32
Luceeee213 <shiyifan213@gmail.com>
2020-08-27 13:35:11 -0700

    add my homework

---
+++ homeworks/dispersal SHI.R
@@ -0,0 +1,31 @@
+# Input: arugments for A, N vector, and arguments for BH
+# output: vector of fish numbers after movement
+out <- A(1, -5, 50, 99)
+N <- numeric(99)
+N[1] <- 100
+BH(N%*%out, 3, 100)
+
+# Next step: Have this run for g generations, and save the output from each generation in a matrix! (loops!)
+disperse_and_grow <- function(N, m, n, L, K, r, g){
+ out <- A(1, -5, 50, 99)
+  N <- numeric(99)
+  N[1] <- 100
+  BH(N%*%out, 3, 100)
+
+  output <- BH(N%*%out, 3, 100)
+
+  for(g in 1 :99){
+    new <- BH(output[g,]%*%out, 3, 100)
+    output <- rbind(output,new)
+    
+  }
+  return(output)
+}
+
+
+# loops
+x <- 101:200
+for(i in 2:100){
+  x[i-1] <- x[i-1] + x[i]
+}
+
