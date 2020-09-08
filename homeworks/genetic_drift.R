d627b6e8afef19ef8af3b1f171dfd8107f5c7906
hemstrow <whemstrom@ucdavis.edu>
2020-09-08 12:39:40 -0700

    added homework part 2, multipop

--- homeworks/genetic_drift.R
+++ homeworks/genetic_drift.R
@@ -23,3 +23,19 @@ plot(nmaf)
 # expand this to do the same process, but with all 10 loci!
 # if you have time, try plotting it
 # hint: binom(10, 100000, seq(from = .1, to = .5, length.out = 10))/100000
+
+
+
+#===============part 2: the part 2ining==========================
+# now, track for multiple populations!
+# first, use a nested loop to do each pop each generation
+# second, vectorize everything to loop only through generations.
+
+# we will need a 3D storage solution(nmaf[gen, locus, population])
+# will use an array
+npop <- 10
+g <- 10
+
+nmaf <- array(NA, c(g, length(imaf), npop))
+## access like anything else
+nmaf[2,,3] # gen 2, every locus, pop 3
