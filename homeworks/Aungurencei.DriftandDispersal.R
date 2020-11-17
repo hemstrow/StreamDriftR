dc952c1e5cba174f4c5613b82818f8a9d270dda4
hemstrow <whemstrom@ucdavis.edu>
2020-11-16 17:25:12 -0800

    added hints

--- homeworks/Aungurencei.DriftandDispersal.R
+++ homeworks/Aungurencei.DriftandDispersal.R
@@ -4,3 +4,10 @@ dm
 N2 <- colSums(dm)
 dm <- mc2d::rmultinomial(nrow(out), N2, t(out))
 
+
+# hints:
+tm <- matrix(1:9, 3, 3) # dispersal numbers, rows are source pop
+afs <- matrix(runif(9, 0, .5), 3, 3) # afs, rows are loci
+draws <- rbinom(3*3*3, rep(tm*2, each = 3), afs) # this will flip coins correctly
+# for locus 1, destination 1, the new allele frequency is:
+sum(draws[c(1, 4, 7)])/ colSums(tm)[1]
